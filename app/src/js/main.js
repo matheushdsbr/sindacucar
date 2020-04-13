07101fd97390a7ea98960cce53cab5ee19380ca7
Matheus Henrique <matheushds.br@gmail.com>
2020-04-13 20:04:49 -0300

    rectoring code

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -3,6 +3,21 @@ $(function () {
 
   $(".slick-cards").slick({
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          autoplay: true,
+          dots: false,
+          arrows: false,
+          autoplaySpeed: 2500,
+        }
+      }
+    ]
   });
 });
